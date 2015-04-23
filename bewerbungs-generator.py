de5caaa0fe2f49febce1522574d5786a6485290a
davidak <git@davidak.de>
2015-04-24 00:13:24 +0200

    add Jinja2 Environment for LaTeX

--- bewerbungs-generator.py
+++ bewerbungs-generator.py
@@ -21,6 +21,23 @@ import jinja2
 
 __version__ = '0.1'
 
+latex_jinja_env = jinja2.Environment(
+	block_start_string = '\BLOCK{',
+	block_end_string = '}',
+	variable_start_string = '\VAR{',
+	variable_end_string = '}',
+	comment_start_string = '\#{',
+	comment_end_string = '}',
+	line_statement_prefix = '%-',
+	line_comment_prefix = '%#',
+	trim_blocks = True,
+	autoescape = False,
+	loader = jinja2.FileSystemLoader(os.path.abspath('templates/'))
+)
+
+template = latex_jinja_env.get_template('test.tex')
+print(template.render(abc='Hello'))
+
 def deckblatt():
 	# -> deckblatt.tex
 	pass
